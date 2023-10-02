a4f7761e55d7674f12f85efd0737739d85003ffb
nba265 <bbb2651999@gmail.com>
2023-10-02 21:40:58 +0700

    Add Login User views and CSS

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Users
+  # Class SessionsController for Users
   class SessionsController < Devise::SessionsController
     # before_action :configure_sign_in_params, only: [:create]
 
