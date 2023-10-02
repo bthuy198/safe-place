0f906428a4ae1a9d8d669a3b0c81bd580c716c9c
nba265 <bbb2651999@gmail.com>
2023-10-02 14:37:42 +0700

    Add Login Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   devise_for :admins
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+
+  namespace :users do
+    resources :users
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
