b9dc89e57eed1284f63486de2144d45edf416fb2
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-10-02 11:15:38 +0700

    Fix annotate messages.

--- app/models/like.rb
+++ app/models/like.rb
@@ -5,7 +5,7 @@
 # Table name: likes
 #
 #  id            :bigint           not null, primary key
-#  anonymous     :boolean
+#  anonymous     :boolean          default(FALSE)
 #  like          :boolean          default(FALSE)
 #  likeable_type :string(255)      not null
 #  created_at    :datetime         not null
