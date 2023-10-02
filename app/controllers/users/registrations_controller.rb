fa1de4c89dca1fa5513ac4b3abb9614e4c1c760c
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-10-02 23:02:28 +0700

    Fix blank layout name. Render user login and registration in blank layout.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,7 @@
 module Users
   # Class RegistrationsController for Users
   class RegistrationsController < Devise::RegistrationsController
+    layout 'blank_layout/blank'
     # before_action :configure_sign_up_params, only: [:create]
     # before_action :configure_account_update_params, only: [:update]
 
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,7 @@
 module Users
   # Class SessionsController for Users
   class SessionsController < Devise::SessionsController
+    layout 'blank_layout/blank'
     # before_action :configure_sign_in_params, only: [:create]
 
     # GET /resource/sign_in
