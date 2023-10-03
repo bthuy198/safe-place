a4d27d62a53a7e57cc57a67c2c0403aa79296c0d
HanNgoBa <bahanngo7@gmail.com>
2023-10-03 14:44:53 +0700

    complete task

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,6 @@ Rails.application.routes.draw do
     resources :user_info
   end
 
-  resources :admins
   
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
