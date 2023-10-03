001ca349a671fd361501c8cf2badbc433fbdc219
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-10-03 21:03:08 +0700

    Generate Devise controller (to render layout).

--- app/controllers/devise/admins/sessions_controller.rb
+++ app/controllers/devise/admins/sessions_controller.rb
@@ -22,13 +22,6 @@ module Devise
       #   super
       # end
 
-      # protected
-
-      # If you have extra params to permit, append them to the sanitizer.
-      # def configure_sign_in_params
-      #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-      # end
-
       def after_sign_in_path_for(_resource)
         admins_dashboard_path
       end
@@ -36,6 +29,13 @@ module Devise
       def after_sign_out_path_for(_resource)
         new_admin_session_path
       end
+
+      # protected
+
+      # If you have extra params to permit, append them to the sanitizer.
+      # def configure_sign_in_params
+      #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+      # end
     end
   end
 end
