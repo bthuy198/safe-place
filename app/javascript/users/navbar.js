c7cd49d206fad487666cb470e0b2584beddaadc6
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-10-05 19:49:22 +0700

    Reverse column in small portview.

---
+++ app/javascript/users/navbar.js
@@ -0,0 +1,25 @@
+function moveForward() {
+    window.history.forward();
+}
+function moveBackward() {
+    window.history.back();
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const targetElement = document.querySelector('.custom_render_row');
+
+    function handleViewportChange(mq) {
+      if (mq.matches) {
+        targetElement.style.display = 'flex';
+        targetElement.style.flexDirection = 'column-reverse';
+      } else {
+        targetElement.style.display = '';
+        targetElement.style.flexDirection = '';
+      }
+    }
+
+    const mq = window.matchMedia('(max-width: 767px)');
+    handleViewportChange(mq);
+
+    mq.addEventListener('change', handleViewportChange);
+});
