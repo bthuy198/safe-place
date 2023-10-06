9cf98c416c719c8a7ad7395f275c647fc5c9d09b
nba265 <bbb2651999@gmail.com>
2023-10-06 18:16:14 +0700

    Fix add Counselor

--- app/controllers/admins/counselors_controller.rb
+++ app/controllers/admins/counselors_controller.rb
@@ -8,7 +8,7 @@ module Admins
     DEFAULT_PASSWORD = '123456'
 
     def index
-      @q = Counselor.includes(:user_info).ransack(params[:q])
+      @q = User.includes(:user_info).ransack(params[:q])
       @counselors = @q.result(distinct: true).order(id: :desc).page params[:page]
     end
 
@@ -18,15 +18,15 @@ module Admins
     end
 
     def create
-      @counselor = Counselor.new(counselor_params)
+      @counselor = User.new(counselor_params)
       @counselor.password = DEFAULT_PASSWORD
-
       respond_to do |format|
         if @counselor.save
           format.html do
             redirect_to @counselor.type == 'User' ? admins_user_url(@counselor) : admins_counselor_url(@counselor),
                         notice: 'counselor was successfully created.'
           end
+          format.turbo_stream
         else
           flash[:alert] = @counselor.errors.full_messages.join(', ')
           format.html { render :new, status: :unprocessable_entity }
@@ -37,7 +37,6 @@ module Admins
     def update
       respond_to do |format|
         if @counselor.update(counselor_params)
-
           format.html do
             redirect_to @counselor.type == 'User' ? admins_user_url(@counselor) : admins_counselor_url(@counselor),
                         notice: 'counselor was successfully updated.'
@@ -55,14 +54,14 @@ module Admins
 
       respond_to do |format|
         format.html do
-          redirect_to admins_counselors_url(page: params[:page]), notice: 'Counselor was successfully destroyed.'
+          redirect_to admins_counselors_url(page: params[:page]), notice: 'User was successfully destroyed.'
         end
         format.json { head :no_content }
       end
     end
 
     def toggle_anonymous
-      @counselor = Counselor.find(params[:id])
+      @counselor = User.find(params[:id])
       @counselor.update(anonymous: params[:anonymous])
       render json: { message: "The 'Anonymous' status has been successfully updated." }
     end
@@ -74,7 +73,7 @@ module Admins
     private
 
     def set_counselor
-      @counselor = Counselor.find(params[:id])
+      @counselor = User.find(params[:id])
     end
 
     def counselor_params
