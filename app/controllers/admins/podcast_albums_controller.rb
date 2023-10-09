87478adc792f2d29ab77c773c3c5dc4c6f73bf82
bthuy198 <bichthuydangthi198@gmail.com>
2023-10-09 15:39:56 +0700

    complete crud admin podcast album

--- app/controllers/admins/podcast_albums_controller.rb
+++ app/controllers/admins/podcast_albums_controller.rb
@@ -3,7 +3,7 @@
 module Admins
 class PodcastAlbumsController < AdminsLayoutController
   before_action :set_podcast_album, only: %i[show edit update destroy]
-
+  before_action :set_default_result
   # GET /podcast_albums or /podcast_albums.json
   def index
     @q = PodcastAlbum.ransack(params[:q])
@@ -32,45 +32,32 @@ class PodcastAlbumsController < AdminsLayoutController
   # POST /podcast_albums or /podcast_albums.json
   def create
     @podcast_album = PodcastAlbum.new(podcast_album_params)
-
-    respond_to do |format|
-      # debugger
-      if @podcast_album.save
-        # binding.pry
-        format.html { redirect_to @podcast_album, notice: 'Podcast album was successfully created.' }
-          # format.html { redirect_to [:admins, @podcast_album], notice: 'Podcast album was successfully created.' }
-        format.js {}
-        format.json { render :show, status: :created, location: @podcast_album }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @podcast_album.errors, status: :unprocessable_entity }
-      end
+    if @podcast_album.save
+      flash[:message] = 'Create successful'
+      @result = { success: true, message: 'Create successful'}
+    else
+      flash[:message] = @podcast_album.errors.full_messages
+      @result = { success: false, message: 'Create failed'}
     end
   end
 
   # PATCH/PUT /podcast_albums/1 or /podcast_albums/1.json
   def update
-    respond_to do |format|
-      if @podcast_album.update(podcast_album_params)
-        format.html { redirect_to @podcast_album, notice: 'Podcast album was successfully updated.' }
-        format.js {}
-        format.json { render :show, status: :ok, location: @podcast_album }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @podcast_album.errors, status: :unprocessable_entity }
-      end
+    if @podcast_album.update(podcast_album_params)
+      flash[:message] = 'Update successful'
+      @result = { success: true, message: 'Update successful'}
+    else
+      # binding.pry
+      flash[:message] = @podcast_album.errors.full_messages
+      @result = { success: false, message: 'Create failed'}
     end
   end
 
   # DELETE /podcast_albums/1 or /podcast_albums/1.json
   def destroy
-    @podcast_album = PodcastAlbum.find(params[:id])
+    # @podcast_album = PodcastAlbum.find(params[:id])
     @podcast_album.destroy
-    respond_to do |format|
-      format.html { redirect_to admins_podcast_albums_url, notice: 'Product was successfully destroyed.' }
-      format.json { head :no_content }
-      format.js   { render layout: false }
-    end
+    redirect_to admins_podcast_albums_url
   end
 
   private
@@ -83,6 +70,10 @@ class PodcastAlbumsController < AdminsLayoutController
   # Only allow a list of trusted parameters through.
   def podcast_album_params
     params.require(:podcast_album).permit(:name, :user_id, :image)
+  end
+
+  def set_default_result
+    @result = { success: false}
   end
 end
 end
