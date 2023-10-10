4349988b641ecee2081fbf8b94643f45c3314bfc
nba265 <bbb2651999@gmail.com>
2023-10-10 16:32:17 +0700

    Add view Album_Details

--- app/controllers/users/album_homepages_controller.rb
+++ app/controllers/users/album_homepages_controller.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
 module Users
-    class AlbumHomepagesController < UsersLayoutController
-      
-      def index
-        @albums = PodcastAlbum.limit(6)
-        @recent_albums = PodcastAlbum.order(created_at: :desc).limit(6)
-      end
+  class AlbumHomepagesController < UsersLayoutController
+    def index
+      @albums = PodcastAlbum.limit(6)
+      @recent_albums = PodcastAlbum.order(created_at: :desc).limit(6)
+    end
 
-      def show
-        @album = PodcastAlbum.find(params[:id])
-      end
-  
+    def show
+      @album = PodcastAlbum.find_by(id: params[:id])
+      @podcasts = @album.podcasts.order(:episode_number)
     end
   end
-  
+end
