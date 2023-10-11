80bff36ff1f32e38436aaaa2c3b6138b759dc6b9
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-10-11 21:57:18 +0700

    Split confessions controller according to rubocop.

---
+++ app/controllers/users/confessions/confessions_controller_callbacks_part1.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+# rubocop:disable Rails/LexicallyScopedActionFilter
+
+module Users
+  module Confessions
+    # module Confessions Controller contain callbacks
+    module ConfessionsControllerCallbacksPart1
+      extend ActiveSupport::Concern
+
+      included do
+        before_action :check_url
+        before_action :set_confessions, only: %i[index destroy]
+        before_action :set_confession, only: %i[like show update destroy]
+        # rubocop:enable Rails/LexicallyScopedActionFilter
+
+        private
+
+        def check_url
+          return unless request.original_url.include?('confessions') && request.referer.nil?
+
+          redirect_to root_path
+        end
+
+        def set_confessions
+          @confessions = Confession.order(created_at: :desc).page(params[:page]).per(3)
+          @current_page = @confessions.current_page
+          @total_pages = @confessions.total_pages
+          @has_next_page = @current_page < @total_pages
+        end
+
+        def set_confession
+          @confession = Confession.find_by(id: params[:id])
+          return unless @confession.nil?
+
+          respond_to do |format|
+            format.html { redirect_to root_path, alert: 'Confession not found.' }
+            format.json { head :no_content }
+          end
+        end
+      end
+    end
+  end
+end
--- db/migrate/20231009103010_remove_like_from_likes.rb
+++ db/migrate/20231009103010_remove_like_from_likes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Remove like column
 class RemoveLikeFromLikes < ActiveRecord::Migration[7.0]
   def up
     remove_column :likes, :like
