4866a1be1634d04a267ba5eb69836fb28737325f
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-10-15 11:31:42 +0700

    Home page search form.

--- app/javascript/users/home_page.js
+++ app/javascript/users/home_page.js
@@ -1,10 +1,3 @@
-function moveForward() {
-    window.history.forward();
-}
-function moveBackward() {
-    window.history.back();
-}
-
 document.addEventListener('DOMContentLoaded', function() {
   const targetElement = document.querySelector('.custom_render_row');
 
@@ -22,6 +15,28 @@ document.addEventListener('DOMContentLoaded', function() {
   handleViewportChange(mq);
 
   mq.addEventListener('change', handleViewportChange);
+
+
+  $('.radio_label').on('click', function(e) {
+    e.stopPropagation();
+  });
+
+
+  const searchForm = document.querySelector('#search_form');
+  const searchOption = document.querySelectorAll('.search_option');
+
+  searchOption.forEach(option => {
+    option.addEventListener('change', function() {
+      if (this.checked) {
+        searchForm.action = this.value;
+        if (searchForm.action.includes('confessions')) {
+          searchForm.setAttribute('data-turbo-frame', 'board');
+        } else if (searchForm.action.includes('podcasts')) {
+          searchForm.setAttribute('data-turbo-frame', 'main');
+        }
+      }
+    });
+  });
 });
 
 document.addEventListener("turbo:frame-missing", function(event) {
