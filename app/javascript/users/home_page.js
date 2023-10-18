c28131dcdefc5cf824aacb8d2db134bc1a87cb2d
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-10-18 16:23:44 +0700

    Fix confession delete action.

--- app/javascript/users/home_page.js
+++ app/javascript/users/home_page.js
@@ -21,3 +21,14 @@ document.addEventListener("turbo:frame-missing", function(event) {
   event.preventDefault();
   event.target.remove();
 });
+
+document.addEventListener("turbo:before-stream-render", (event) => {
+  if(event.detail.newStream.getAttribute("action")=="remove") {
+    let target_id = event.detail.newStream.getAttribute("target")
+    if(target_id.includes("confession")) {
+      let target = document.getElementById(target_id)
+    let modal = target.children[2].children[2]
+    $(modal).modal("hide")
+    }
+  }
+})
