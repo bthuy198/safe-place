29c05068e8a0f11e029b44193aa94c6595009709
nba265 <bbb2651999@gmail.com>
2023-10-19 14:00:45 +0700

    Fix Conflict

--- app/controllers/users/album_homepages_controller.rb
+++ app/controllers/users/album_homepages_controller.rb
@@ -11,6 +11,7 @@ module Users
     def show
       @album = PodcastAlbum.find_by(id: params[:id])
       @podcasts = @album.podcasts.order(:episode_number)
+      render layout: false
     end
 
     def all_album
---
+++ app/javascript/users/podcast_albums/albums_show_page.js
@@ -0,0 +1,70 @@
+var playPauseIconAlbum = $("#play-pause-icon-album");
+var listPodcastItem = $(".podcast-item");
+var listPodcastItemArrays = listPodcastItem.map(function () { return $(this).data('audio')}).get();
+var listLikedPodcastItem = $("input[name='liked_podcast']").map(function () {return parseInt(this.value)}).get();
+var listBookmarkedPodcastItem = $("input[name='bookmarked_podcast']").map(function() {return parseInt(this.value)}).get();
+
+listPodcastItem.on("click", function () {
+  let index = listPodcastItem.index(this);
+  if (!checkIfIncludeAudio(currentPodcast, listPodcastItem)) {
+    loadNewPlaylistPodcast(listPodcastItemArrays, index);
+  } else {
+    loadPodcast(index);
+  }
+  playPauseIconAlbum
+    .removeClass("fa-circle-play play")
+    .addClass("fa-circle-pause pause");
+});
+
+playPauseIconAlbum.on("click", function () {
+  if (playPauseIconAlbum.hasClass("play")) {
+    if (!checkIfIncludeAudio(currentPodcast, listPodcastItem)) {
+      loadNewPlaylistPodcast(listPodcastItemArrays, 0);
+    } else {
+      audioPlayer.play();
+    }
+    playPauseIconAlbum
+      .removeClass("fa-circle-play play")
+      .addClass("fa-circle-pause pause");
+  } else {
+    audioPlayer.pause();
+    playPauseIconAlbum
+      .removeClass("fa-circle-pause pause")
+      .addClass("fa-circle-play play");
+  }
+});
+
+$(".bookmark_podcast_albums").on("click", function () {
+    handleBookmarkPodcastAlbumsClick($(this));
+});
+
+$(".heart_podcast_albums").on("click", function () {
+    handleLikePodcastAlbumsClick($(this));
+});
+
+function updatePlayPauseAblumIcon() {
+  if (checkIfIncludeAudio(currentPodcast, listPodcastItem) && !audioPlayer.paused) {
+    playPauseIconAlbum
+      .removeClass("fa-circle-play play")
+      .addClass("fa-circle-pause pause");
+  } else {
+    playPauseIconAlbum
+      .removeClass("fa-circle-pause pause")
+      .addClass("fa-circle-play play");
+  }
+}
+
+function checkIfIncludeAudio(element, listElement) {
+  if (typeof element != "undefined" && podcasts.length != 0) {
+    for (var i = 0; i < listElement.length; i++) {
+      if ($(listElement[i]).data("audio").id === element.id) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+$("#play-pause-icon").on("click", updatePlayPauseAblumIcon);
+
+updatePlayPauseAblumIcon();
