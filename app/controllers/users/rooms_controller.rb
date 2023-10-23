f4e26cec00efad078eb509869ee21c1758c8f11d
hovannam100901 <nam.work@gmail.com>
2023-10-23 09:46:02 +0700

    done function of counselor

--- app/controllers/users/rooms_controller.rb
+++ app/controllers/users/rooms_controller.rb
@@ -11,7 +11,11 @@ module Users
     end
 
     def show
-      @room = Room.find_by(id: params[:id], user_id: current_user.id)
+      if current_user.type == "User"
+        @room = Room.find_by(id: params[:id], user_id: current_user.id)
+      else
+        @room = Room.find_by(id: params[:id], counselor_id: current_user.id)
+      end
       render layout: "blank_layout/user_blank"
     end
 
