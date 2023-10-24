d1db482d884092222413891a6e97f4bdc5f8db82
hovannam100901 <146151449+hovannam100901@users.noreply.github.com>
2023-10-24 15:33:35 +0700

    Update confessions_controller_destroy_action.rb

--- app/controllers/users/confessions/confessions_controller_destroy_action.rb
+++ app/controllers/users/confessions/confessions_controller_destroy_action.rb
@@ -8,20 +8,12 @@ module Users
 
       included do
         def destroy
+          @confession.destroy
           respond_to do |format|
-            if current_user.id == @confession.user_id
-              @confession.destroy
-              format.turbo_stream do
-                flash.now[:notice] = 'Confession was successfully destroyed.'
-              end
-              format.json { head :no_content }
-              Turbo::StreamsChannel.broadcast_remove_to('confessions_index_channel',
-                                                        target: helpers.dom_id(@confession))
-            else
-              format.turbo_stream do
-                flash.now[:alert] = "Cannot delete other people's confessions"
-              end
+            format.turbo_stream do
+              flash.now[:notice] = 'Confession was successfully destroyed.'
             end
+            format.json { head :no_content }
           end
           broadcast_destroy_confession
         end
