09a73c9affc4481d703626de78bc87c727bd16a2
hovannam100901 <nam.work@gmail.com>
2023-10-24 17:57:34 +0700

    import data for project

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,92 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+# create user and user_inf
+5.times do |i|
+    User.create!(
+        email: Faker::Internet.email,
+        user_name: Faker::Name.name,
+        password: "123123", 
+        password_confirmation: "123123",
+        type: %w[User Counselor].sample,
+        user_info_attributes: {
+            address: Faker::Address.street_address,
+            date_of_birth: Faker::Date.birthday,
+            profile_name: Faker::Name.name,
+            gender: UserInfo.genders.keys.sample,
+        }
+    )
+end
+
+# create confession and comment for confession
+20.times do |i|
+    user = User.all.sample
+    con = Confession.create!(
+        tag: Faker::Lorem.words,
+        content: Faker::Lorem.sentence,
+        anonymous: user.anonymous,
+        user_id: user.id,
+    )
+    3.times do |j|
+        con.comments.create!(
+            anonymous: user.anonymous,
+            user_id: user.id,
+            comment: Faker::Lorem.sentence
+        )
+    end
+end
+
+# create room chat and conversation for this room chat
+10.times do |i|
+    room = Room.create(
+        name: Faker::Space.star_cluster,
+        user_id: User.where(type:"User").sample.id,
+        counselor_id: Counselor.all.sample.id,
+        status: User.statuses.keys.sample
+    )
+    3.times do |j|
+        user = User.all.sample
+        user.conversations.create(
+            room_id: room.id, 
+            content: Faker::Lorem.sentence
+        )
+    end
+end
+
+# create podcastalbum and podcast
+20.times do |i|
+    album = PodcastAlbum.create(
+        name: Faker::Music.album,
+        user_id: User.all.sample.id,
+    )
+    3.times do |j|
+        podcast = Podcast.create(
+            name: Faker::Music.album,
+            podcast_album_id: album.id,
+            author_name: Faker::Music.band,
+            episode_number: Faker::Number.between(from: 1, to: 100),
+            audio: File.open("/home/msi/Downloads/file_example_MP3_700KB.mp3")
+        )
+    end
+
+end
+
+# create like for Polymorphic(PodcastAlbum, Podcast, Confession)
+5.times do |i|
+    user = User.all.sample
+    album = PodcastAlbum.all.sample
+    podcast = Podcast.all.sample
+    confession = Confession.all.sample
+
+    album.likes.create(
+        anonymous: user.anonymous,
+        user_id: user.id,
+    )
+    podcast.likes.create(
+        anonymous: user.anonymous,
+        user_id: user.id,
+    )
+    confession.likes.create(
+        anonymous: user.anonymous,
+        user_id: user.id,
+    )
+end
