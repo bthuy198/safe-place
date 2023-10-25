b28eaa36ece7b902c5b267c88729f316d8207961
Nguyen Khoa Hoang Phuc <nkhphuc@gmail.com>
2023-10-25 17:46:14 +0700

    Confessions modal close after delete (using class).

--- app/javascript/users/home_page.js
+++ app/javascript/users/home_page.js
@@ -27,9 +27,8 @@ document.addEventListener("turbo:before-stream-render", (event) => {
     let target_id = event.detail.newStream.getAttribute("target")
     if(target_id.includes("confession")) {
       let target = document.getElementById(target_id)
-      console.log(target)
-    let modal = target.children[3].children[2]
-    $(modal).modal("hide")
+      let modal = target.querySelector(".confession_modal")
+      $(modal).modal("hide")
     }
   }
 })
